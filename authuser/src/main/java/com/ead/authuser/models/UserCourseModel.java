a01649300ba8f19e87564d2f5e564c733441b6f5
cairomedeiros <cairomedeiros13@gmail.com>
2024-12-24 15:00:10 -0300

    save subscription user in one course

--- authuser/src/main/java/com/ead/authuser/models/UserCourseModel.java
+++ authuser/src/main/java/com/ead/authuser/models/UserCourseModel.java
@@ -2,13 +2,17 @@ package com.ead.authuser.models;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.util.UUID;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "TB_USERS_COURSES")
 public class UserCourseModel implements Serializable {
