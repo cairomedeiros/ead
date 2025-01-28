c83c1b962531d25afa43c2dfb07793603e766c04
cairomedeiros <cairomedeiros13@gmail.com>
2025-01-27 21:12:22 -0300

    preparing for asynchronous communication

--- course/src/main/java/com/ead/course/models/UserModel.java
+++ course/src/main/java/com/ead/course/models/UserModel.java
@@ -1,7 +1,9 @@
 package com.ead.course.models;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,15 +16,10 @@ import java.util.UUID;
 @NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Entity
-@Table(name = "TB_COURSES_USERS")
-public class CourseUserModel implements Serializable {
+@Table(name = "TB_USERS")
+public class UserModel implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private UUID id;
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    private CourseModel course;
-    @Column(nullable = false)
     private UUID userId;
 }
